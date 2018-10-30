6ad2ae7c963e508b383f0b14131b066362b1f57e
Sam <samanthajeet@gmail.com>
2018-10-29 17:32:46 -0700

    age change

--- assessment.js
+++ assessment.js
@@ -23,7 +23,7 @@
 // CODE HERE
 
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
-var age = 18;
+var age = 21;
 var canDrive;
 
 
